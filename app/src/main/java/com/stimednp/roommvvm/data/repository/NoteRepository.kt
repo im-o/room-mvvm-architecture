4d7c25bb4e3bc78c965e1406a8fc727037d244e2
im-o <frizgt@gmail.com>
2020-10-19 10:17:37 +0800

    :zap: add viewmodel and repository to call function from NoteDao

---
+++ app/src/main/java/com/stimednp/roommvvm/data/repository/NoteRepository.kt
@@ -0,0 +1,27 @@
+package com.stimednp.roommvvm.data.repository
+
+import androidx.lifecycle.LiveData
+import com.stimednp.roommvvm.data.db.entity.Note
+import com.stimednp.roommvvm.data.db.NoteDatabase
+
+/**
+ * Created by rivaldy on Oct/18/2020.
+ * Find me on my lol Github :D -> https://github.com/im-o
+ */
+
+class NoteRepository(
+    private val noteDatabase: NoteDatabase
+) {
+
+    suspend fun insertNote(note: Note) = noteDatabase.getNoteDao().insertNote(note)
+
+    suspend fun updateNote(note: Note) = noteDatabase.getNoteDao().updateNote(note)
+
+    suspend fun deleteNote(note: Note) = noteDatabase.getNoteDao().deleteNote(note)
+
+    suspend fun deleteNoteById(id: Int) = noteDatabase.getNoteDao().deleteNoteById(id)
+
+    suspend fun clearNote() = noteDatabase.getNoteDao().clearNote()
+
+    fun getAllNotes(): LiveData<List<Note>> = noteDatabase.getNoteDao().getAllNotes()
+}
