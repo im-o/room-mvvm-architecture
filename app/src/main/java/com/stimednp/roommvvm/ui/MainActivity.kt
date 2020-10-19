e24e34596eb206db303dc7e06bf00550c92c6f41
im-o <frizgt@gmail.com>
2020-10-19 12:42:42 +0800

    :zap: update ui and add adapter to show data from database

--- app/src/main/java/com/stimednp/roommvvm/ui/MainActivity.kt
+++ app/src/main/java/com/stimednp/roommvvm/ui/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.stimednp.roommvvm.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stimednp.roommvvm.R
+import com.stimednp.roommvvm.data.db.NoteDatabase
+import com.stimednp.roommvvm.data.repository.NoteRepository
+import com.stimednp.roommvvm.utils.Coroutines
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: NoteViewModel
+    private lateinit var noteDatabase: NoteDatabase
+    private lateinit var repository: NoteRepository
+    private lateinit var factory: NoteViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //bad practice because boilerplate code, but we'll be change this later using DI.
+        noteDatabase = NoteDatabase(this)
+        repository = NoteRepository(noteDatabase)
+        factory = NoteViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory)[NoteViewModel::class.java]
+
+        initClick()
+        showData()
+    }
+
+    private fun initClick() {
+        addNoteFAB.setOnClickListener {
+            //@todo move to form here
+        }
+    }
+
+    private fun showData() {
+        Coroutines.main {
+            viewModel.getAllNotes().observe(this@MainActivity, {
+                notesRV.apply {
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+                    setHasFixedSize(true)
+                    adapter = NoteAdapter(it)
+                }
+            })
+        }
     }
 }
---
+++ app/src/main/java/com/stimednp/roommvvm/utils/UtilFunctions.kt
@@ -0,0 +1,14 @@
+package com.stimednp.roommvvm.utils
+
+import android.util.Log
+
+/**
+ * Created by rivaldy on Oct/19/2020.
+ * Find me on my lol Github :D -> https://github.com/im-o
+ */
+
+object UtilFunctions {
+    fun loge(message: String) {
+        Log.e("THIS ERROR", "ERROR -> $message")
+    }
+}
